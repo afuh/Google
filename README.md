8d551162d28f112725dd874ddac2607d631f2452
Axel <axelfuh@gmail.com>
2017-02-09 22:33:44 +0100

    Header

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# google
+# Google homepage
+The Odin Project
---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+$(document).ready(function(){
+  var inpt = [], url;
+  $("form").submit(function(){
+    inpt = $("#searchBox").val();
+    url = "https://www.google.com/search?q=" + inpt;
+    window.open(url);
+  });
+
+
+});
+*/
